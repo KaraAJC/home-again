6aaff7ad57529835410331f5e8e8e2b2d601da1c
Mike Chu <m.chu90@gmail.com>
2016-06-11 14:20:34 -0500

    added event listener from app.js and added SMS controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,9 +2,41 @@ angular.module('starter.controllers', ['ionic', 'ngCordova', 'starter.controller
 
 
 .controller('SMSController', function($scope, $cordovaSms) {
+  $scope.sms = {
+      number: '0959052082',
+      message: 'This is some dummy text'
+    };
+
+    document.addEventListener("deviceready", function() {
+
+      var options = {
+        replaceLineBreaks: false, // true to replace \n by a new line, false by default
+        android: {
+          intent: '' // send SMS with the native android SMS messaging
+            //intent: '' // send SMS without open any other app
+            //intent: 'INTENT' // send SMS inside a default SMS app
+        }
+      };
+
+      $scope.sendSMS = function() {
+
+        $cordovaSms
+          .send('0959052082', 'This is some dummy text', options)
+          .then(function() {
+            alert('Success');
+            // Success! SMS was sent
+          }, function(error) {
+            alert('Error');
+            // An error occurred
+          });
+      }
+    });
 })
 
 .controller('DashCtrl', function($scope) {  
+})
+
+.controller('SmsCtrl', function($scope) {
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
